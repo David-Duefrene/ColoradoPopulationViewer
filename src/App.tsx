32a4473da1eb9f37c59aac630231f1f09bf5223d
David Duefrene <fortismilite@gmail.com>
2022-07-28 11:15:13 -0600

    Make set county be a list of counties

--- src/App.tsx
+++ src/App.tsx
@@ -5,39 +5,50 @@ import ChartBox from './ChartBox/ChartBox';
 
 import './App.css'
 
+interface ChartData {
+	date: number;
+	value: number;
+}
+
 function App() {
-	const [county, setCounty] = useState('Denver');
+	const [county, setCounty] = useState(['Denver']);
 	const countyList = Object.keys(data).map(entry => {
 		return (
-			<li>
+			<li key={entry}>
 				<button
-					className={`SelectionButton ${county == entry ? 'Active' : ''}`}
-					onClick={() => setCounty(entry)}
+					className={`SelectionButton ${county.includes(entry) ? 'Active' : ''}`}
+					onClick={() => {
+						if (county.includes(entry)) { return; }
+						setCounty([...county, entry]);
+					}}
 				>
 					{entry}
 				</button>
 			</li>
 		);
 	});
-	const dataSet = [];
+	const dataSets: any = {};
 
-	// https://stackoverflow.com/questions/57409041/element-implicitly-has-an-any-type-because-expression-of-type-string-number
-	// @ts-ignore
-	for (const dataPoint of data[county]) {
-		dataSet.push({
-			date: dataPoint.year,
-			value: dataPoint.totalPopulation
-		});
-	};
+	county.forEach(entry => {
+		dataSets[entry] = [];
+		// https://stackoverflow.com/questions/57409041/element-implicitly-has-an-any-type-because-expression-of-type-string-number
+		// @ts-ignore
+		for (const dataPoint of data[entry]) {
+			dataSets[entry].push({
+				date: dataPoint.year,
+				value: dataPoint.totalPopulation
+			});
+		};
 
-	dataSet.sort((a, b) => a.date - b.date);
+		dataSets[entry].sort((a: ChartData, b: ChartData) => a.date - b.date);
+	});
 
 	return (
 		<div className="App">
 			<div className="SidePanel">
 				<ul className='SelectionList'>{countyList}</ul>
 			</div>
-			<ChartBox data={dataSet} />
+			<ChartBox data={dataSets[Object.keys(dataSets)[0]]} />
 		</div>
 	)
 }
